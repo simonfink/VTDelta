c530698c1e3cfb028442cdcbf5392c56d9351255
simon <simon.fink@ntb.ch>
2018-10-08 15:17:20 +0200

    init

---
+++ app/sequence/MainSequence.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <eeros/sequencer/Sequence.hpp>
+#include <eeros/safety/SafetySystem.hpp>
+#include <eeros/sequencer/Monitor.hpp>
+
+#include "../control/DeltaControlSystem.hpp"
+#include "../safety/DeltaSafetyProperties.hpp"
+#include "../conditions/MoveMouseCondition.hpp"
+#include "CalibrateSequence.hpp"
+#include "SortSequence.hpp"
+#include "ShuffleSequence.hpp"
+#include "ExceptionSequence.hpp"
+
+#include <array>
+
+namespace eeduro {
+	namespace delta {
+		class MainSequence : public eeros::sequencer::Sequence {
+		public:
+			MainSequence(std::string name, eeros::sequencer::Sequencer& sequencer, DeltaControlSystem& controlSys, eeros::safety::SafetySystem& safetySys, DeltaSafetyProperties properties, Calibration& calibration);
+			
+			int action();
+
+			
+		private:
+			
+			DeltaControlSystem& controlSys;
+			eeros::safety::SafetySystem& safetySys;
+			DeltaSafetyProperties& properties;
+			
+ 			CalibrateSequence calibSeq;
+			SortSequence sortSeq;
+			ShuffleSequence shuffSeq;
+			MouseSequence mouseSeq;
+			
+			MouseExceptionSequence mexSeq;
+			
+			Calibration& calibration;
+			
+			eeros::sequencer::Monitor mouseMove;
+			MoveMouseCondition mmc;
+			
+			eeros::sequencer::Sequencer& sequencer;
+		};
+	}
+}
+
---
+++ app/sequence/MouseSequence.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <eeros/sequencer/Sequence.hpp>
+#include <eeros/safety/SafetySystem.hpp>
+
+#include "../control/DeltaControlSystem.hpp"
+#include "step/move.hpp"
+#include "step/detect.hpp"
+#include "MoveBlockSequence.hpp"
+#include "ExceptionSequence.hpp"
+#include "step/emag.hpp"
+
+#include <array>
+
+namespace eeduro {
+	namespace delta {
+		class MouseSequence : public eeros::sequencer::Sequence {
+		public:
+			MouseSequence(std::string name, eeros::sequencer::Sequencer& sequencer, eeros::sequencer::BaseSequence* caller, DeltaControlSystem& controlSys, eeros::safety::SafetySystem& safetySys, Calibration& calibration, DeltaSafetyProperties& properties);
+			
+			int action();
+			
+			bool checkPreCondition();
+
+			
+		private:
+			int count;
+			DeltaControlSystem& controlSys;
+			eeros::safety::SafetySystem& safetySys;
+			bool buttonPressed = false;
+			AxisVector mouseNew;
+			AxisVector mouseOld;
+			MouseTimeOutExceptionSequence timeoutSeq;
+			Emag emag;
+		};
+	}
+}
